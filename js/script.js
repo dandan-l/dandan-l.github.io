058b530722e2aedfdab8b139af6d8f41936307f6
dandan502 <danliu.5157@gmail.com>
2022-01-18 14:45:02 -0700

    added home page, about page, peerbots page still in progress

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
-// code credit: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_gallery
+$(document).ready(function () {
+  //Js to highlight the location of the current page
+  //Code credit: https://stackoverflow.com/users/2854760/raj, https://stackoverflow.com/users/4635388/rohan-khude, https://stackoverflow.com/questions/4626431/highlight-the-navigation-menu-for-the-current-page
+  // this will get the full URL at the address bar
+  var url = window.location.href;
+  var hash = window.location.hash;
 
-var slideIndex = 1;
-showSlides(slideIndex);
+  // console.log(url);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+  // passes on every "a" tag
+  $(".nav-web a").each(function () {
+    // console.log(this.href);
+    // checks if its the same on the address bar
+    if (url == this.href) {
+      $(this).closest("li").addClass("active");
+      //for making parent of submenu active
+      $(this).closest("li").parent().parent().addClass("active");
+    }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+    //checks if it's currently on the project page
+    if (
+      url == "https://dandan-l.github.io/closeli.html" ||
+      url == "https://dandan-l.github.io/peerbots.html"
+    ) {
+      $(".work-lnk").closest("li").addClass("active");
+      //for making parent of submenu active
+      $(".work-lnk").closest("li").parent().parent().addClass("active");
+    }
+  });
+});
